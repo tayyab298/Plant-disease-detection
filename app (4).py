885dda8fd7a268cc2100acb499eab2bd9348c2ca
tayyab298 <77976615+tayyab298@users.noreply.github.com>
2023-08-21 22:52:42 +0530

    Add files via upload

---
+++ app (4).py
@@ -0,0 +1,165 @@
+import streamlit as st
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+
+# Define the custom ResNet-18 model for plant disease detection
+class Block(nn.Module):
+    # (in_channels, out_channels, identity_downsample, stride)
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+        super(Block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.identity_downsample = identity_downsample
+
+    def forward(self, x):
+        identity = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+        x += identity
+        x = self.relu(x)
+        return x
+
+class ResNet_18(nn.Module):
+    def __init__(self, image_channels, num_classes):
+
+        super(ResNet_18, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        #resnet layers
+        self.layer1 = self.__make_layer(64, 64, stride=1)
+        self.layer2 = self.__make_layer(64, 128, stride=2)
+        self.layer3 = self.__make_layer(128, 256, stride=2)
+        self.layer4 = self.__make_layer(256, 512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def __make_layer(self, in_channels, out_channels, stride):
+
+        identity_downsample = None
+        if stride != 1:
+            identity_downsample = self.identity_downsample(in_channels, out_channels)
+
+        return nn.Sequential(
+            Block(in_channels, out_channels, identity_downsample=identity_downsample, stride=stride),
+            Block(out_channels, out_channels)
+        )
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+    def identity_downsample(self, in_channels, out_channels):
+
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(out_channels)
+        )
+
+
+# Load the pre-trained model checkpoint for inference
+checkpoint_path = 'plant_disease_resnet18.pth'
+model = ResNet_18(image_channels=3, num_classes=38)  # Assuming 38 classes for plant diseases
+model.load_state_dict(torch.load(checkpoint_path))
+model.eval()
+
+# Class names for plant diseases
+CLASS_NAMES = (
+    'Apple___Black_rot',
+    'Apple___Cedar_apple_rust',
+    'Apple___healthy',
+    'Background_without_leaves',
+    'Blueberry___healthy',
+    'Cherry___Powdery_mildew',
+    'Cherry___healthy',
+    'Corn___Cercospora_leaf_spot Gray_leaf_spot',
+    'Corn___Common_rust',
+    'Corn___Northern_Leaf_Blight',
+    'Corn___healthy',
+    'Grape___Black_rot',
+    'Grape___Esca_(Black_Measles)',
+    'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+    'Grape___healthy',
+    'Orange___Haunglongbing_(Citrus_greening)',
+    'Peach___Bacterial_spot',
+    'Peach___healthy',
+    'Pepper,_bell___Bacterial_spot',
+    'Pepper,_bell___healthy',
+    'Potato___Early_blight',
+    'Potato___Late_blight',
+    'Potato___healthy',
+    'Raspberry___healthy',
+    'Soybean___healthy',
+    'Squash___Powdery_mildew',
+    'Strawberry___Leaf_scorch',
+    'Strawberry___healthy',
+    'Tomato___Bacterial_spot',
+    'Tomato___Early_blight',
+    'Tomato___Late_blight',
+    'Tomato___Leaf_Mold',
+    'Tomato___Septoria_leaf_spot',
+    'Tomato___Spider_mites Two-spotted_spider_mite',
+    'Tomato___Target_Spot',
+    'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+    'Tomato___Tomato_mosaic_virus',
+    'Tomato___healthy'
+)
+
+# Setting Title of App
+st.title("Plant Disease Detection")
+st.markdown("Upload an image of the plant leaf")
+
+# Uploading the image
+plant_image = st.file_uploader("Choose an image...", type="jpg")
+submit = st.button('Predict Disease')
+
+# On predict button click
+if submit:
+    if plant_image is not None:
+        # Read the uploaded image and convert it to a PyTorch tensor
+        image = Image.open(plant_image).convert("RGB")
+        transform = transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+        ])
+        input_tensor = transform(image).unsqueeze(0)
+
+        # Make Prediction
+        with torch.no_grad():
+            output = model(input_tensor)
+            probabilities = torch.softmax(output, dim=1)
+            predicted_class_index = torch.argmax(probabilities, dim=1).item()
+            result = CLASS_NAMES[predicted_class_index]
+
+        st.image(image, channels="RGB")
+        st.write(image.size)
+        st.title("This is a " + result.split('___')[1] + " leaf with " + result.split('___')[0])
